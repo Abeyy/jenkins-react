fab499636ac470fd36953b16c114d82c0acb8d95
Abeyy <abey.joy21@gmail.com>
2022-12-26 01:16:34 -0800

    test change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          This is a change. Does Netlify deploy it?
+          This is a change. Does S3 get it?
         </p>
         <a
           className="App-link"
